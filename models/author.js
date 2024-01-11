e518827221ee5dfded789a4be53f60d431bc35e6
KelvinHJK <kelvinhjk15204@gmail.com>
2024-01-11 11:31:49 +0800

    cp - Author routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Game = require("./game");
 
 const authorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Author', authorSchema);
+authorSchema.pre("deleteOne", function (next) {
+  // this.model('Game').deleteMany({ author: this._id }, next);
+
+  Game.find({ author: this.id }, (err, games) => {
+    if (err) {
+      next(err);
+    } else if (games.length > 0) {
+      next(new Error("This author has games still"));
+    } else {
+      next();
+    }
+  });
+});
+
+module.exports = mongoose.model("Author", authorSchema);
